801dd1e21f51ab1ff818d08c1260f0f5f7e499ce
Avinashkumar8576 <avinashkumar90089008@gmail.com>
2024-06-17 22:36:48 +0530

    Add files via upload

---
+++ src/com/javabasic/Comment.java
@@ -0,0 +1,36 @@
+package javacomment;
+
+public class Comment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Main method starts from here
+		// TODO Auto-generated method stub
+
+			System.out.println("Using Comments in Java");
+			
+			//this is a single comment
+			
+			/*
+			 * This is multiline Comments
+			 * 
+			 */
+			
+			/**
+			 * This is Java Documentation Comments.
+			 * But unlike multiline comments it can be embedded with functional elements which can function properlu
+			 */
+			
+				
+				//Java program execution starts from Main Method
+				
+				// Java comments are used to describe the written code
+				
+				//below line is used to print Hello guys
+				
+			System.out.println("Hello Guys");
+
+		
+	}
+
+}
