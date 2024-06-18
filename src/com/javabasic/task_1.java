b68fef991c72fdf521db8d2d55773f507dd60653
Avinashkumar8576 <avinashkumar90089008@gmail.com>
2024-06-18 11:16:29 +0530

    Add files via upload

---
+++ src/com/javabasic/task_1.java
@@ -0,0 +1,24 @@
+package javacomment  ;
+
+//write java program to print your own information
+public class task_1 {//class startedhere
+    //Java program execution starts from main()method
+	public static void main(String[] args) {
+		// main()method starts from here
+		String Name = "Avinash kumar";
+		System.out.println(Name);//prints name 
+		String Graduation = "Electronic and communication Engineering";
+		System.out.println(Graduation);// prints name of graduation degree
+		int yearofpassing = 2023;
+		System.out.println(yearofpassing);//prints year of passing from college
+		boolean isgraduated = true;
+		System.out.println(isgraduated);// prints true if graduation pass, prints false if graduation failed
+		String Address = "kharadi Pune";
+		System.out.println(Address);// prints from where you are means address
+	}//main() method ends here
+
+} // class ends here
+
+
+
+
